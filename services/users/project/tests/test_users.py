9ae53cb71f56bfa9b0469683bae8bb8646af05fc
sleepybjr <22383876+sleepybjr@users.noreply.github.com>
2019-02-17 12:10:53 -0800

    add jinja templating

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -163,6 +163,34 @@ class TestUserService(BaseTestCase):
 
             self.assertIn('success', data['status'])
 
+    def test_main_no_users(self):
+        """Ensure the main route behaves correctly when no users have been added to the database."""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'All Users', response.data)
+        self.assertIn(b'<p>No users!</p>', response.data)
+
+    def test_main_with_userse(self):
+        """Ensure the main route behaves correctly when users have been added to the database."""
+        add_user('test', 'test@test.com')
+        add_user('user', 'user@user.com')
+        with self.client:
+            response = self.client.get('/')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'All Users', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'test', response.data)
+            self.assertIn(b'user', response.data)
+
+    def test_main_add_user(self):
+        """Ensure a new user can be added to the database via a POST request."""
+        with self.client:
+            response = self.client.post('/', data=dict(username='test', email='test@test.com', follow_redirects=True))
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'All Users', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'test', response.data)
+
 
 if __name__ == '__main__':
     unittest.main()
