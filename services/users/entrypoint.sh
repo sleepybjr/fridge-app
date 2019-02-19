b21073c3eac948d09633467a4c638fd9edb8a93f
sleepybjr <22383876+sleepybjr@users.noreply.github.com>
2019-02-18 19:51:52 -0800

    fix travis ci build
    attempt to fix build 2
    
    make entrypoints executable and revert dockerfile-dev
    
    revert dockerfile-dev and add logging to travis
    
    potentially fix travis and postgres build issue??
    
    fix travis ci postgres issue further with a sleep timer of 2
    
    fix build issue
    
    fixed??
    
    fully fix travis ci

--- services/users/entrypoint.sh
+++ services/users/entrypoint.sh
@@ -2,10 +2,10 @@
 
 echo "Waiting for postgres..."
 
-while ! nc -z users-db 5432; do
-    sleep 0.1
-done
+ while ! nc -z users-db 5432; do
+     sleep 0.1
+ done
 
-echo "PostgreSQL started"
+ echo "PostgreSQL started"
 
 python manage.py run -h 0.0.0.0
