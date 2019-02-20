0023b61e2afaad0560d9c57b24e1cd04ff60d721
sleepybjr <22383876+sleepybjr@users.noreply.github.com>
2019-02-19 17:22:39 -0800

    final travis ci configuration commit

--- services/users/entrypoint.sh
+++ services/users/entrypoint.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 
-echo "Waiting for postgres..."
+echo $(date -u) "Waiting for Postgres..."
 
 while ! nc -z users-db 5432; do
  sleep 0.1
 done
 
-#while ! pg_isready -h users-db -p 5432 -q -U postgres; do
-#  >&2 echo $(date -u) "Postgres is unavailable - sleeping"
-#  sleep 1
-#done
-#
-#echo $(date -u) "Sleep started."
-#sleep 15
-#echo $(date -u) "Sleep ended."
-
->&2 echo $(date -u) "PostgreSQL started"
+echo $(date -u) "PostgreSQL started."
 
 python manage.py run -h 0.0.0.0
