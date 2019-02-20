9c38fb11498f1723531a2ba6930160976a171c91
sleepybjr <22383876+sleepybjr@users.noreply.github.com>
2019-02-19 16:43:13 -0800

    change configuration for travis ci

--- services/users/entrypoint.sh
+++ services/users/entrypoint.sh
@@ -2,10 +2,12 @@
 
 echo "Waiting for postgres..."
 
- while ! nc -z users-db 5432; do
-     sleep 0.1
- done
+while ! nc -z users-db 5432; do
+ sleep 0.1
+done
 
- echo "PostgreSQL started"
+sleep 10
+
+echo "PostgreSQL started"
 
 python manage.py run -h 0.0.0.0
