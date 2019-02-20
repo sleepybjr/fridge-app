55a329a2b47adc34fcdb59bc8266b750ecedfd28
sleepybjr <22383876+sleepybjr@users.noreply.github.com>
2019-02-19 17:16:42 -0800

    just add a long sleep timer..

--- services/users/entrypoint.sh
+++ services/users/entrypoint.sh
@@ -2,18 +2,18 @@
 
 echo "Waiting for postgres..."
 
-#while ! nc -z users-db 5432; do
-# sleep 0.1
-#done
-
-while ! pg_isready -h users-db -p 5432 -q -U postgres; do
-  >&2 echo $(date -u) "Postgres is unavailable - sleeping"
-  sleep 1
+while ! nc -z users-db 5432; do
+ sleep 0.1
 done
 
-echo $(date -u) "Sleep started."
-sleep 15
-echo $(date -u) "Sleep ended."
+#while ! pg_isready -h users-db -p 5432 -q -U postgres; do
+#  >&2 echo $(date -u) "Postgres is unavailable - sleeping"
+#  sleep 1
+#done
+#
+#echo $(date -u) "Sleep started."
+#sleep 15
+#echo $(date -u) "Sleep ended."
 
 >&2 echo $(date -u) "PostgreSQL started"
 
